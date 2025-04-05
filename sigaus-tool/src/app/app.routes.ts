233da889285ad98cdd27f70daf2ea97e7abcb681
OtiliaNicola <otilianicola94@gmail.com>
2025-04-05 12:14:41 +0200

    feat: add routes

--- sigaus-tool/src/app/app.routes.ts
+++ sigaus-tool/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DeclarationsComponent } from './pages/declarations/declarations.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/gestion/declaraciones', pathMatch: 'full' },
+  { path: 'gestion/declaraciones', component: DeclarationsComponent },
+];
