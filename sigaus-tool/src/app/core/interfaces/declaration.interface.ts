3c0aa9d40962dad9c58f8066792d7f76778b7727
OtiliaNicola <otilianicola94@gmail.com>
2025-04-05 18:44:18 +0200

    feat: create interface

---
+++ sigaus-tool/src/app/core/interfaces/declaration.interface.ts
@@ -0,0 +1,14 @@
+export interface Declaration {
+    id: number;
+    reference: string;
+    type: string;
+    date: string;
+    origin: string;
+    regenerable: string;
+    ler: string;
+    quantity: string;
+    oleoso: string;
+    pdf: string;
+    incident: string;
+    actions: string;
+}
