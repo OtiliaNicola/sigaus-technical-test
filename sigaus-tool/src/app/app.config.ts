5ab7965e1006f1a7231503007a7fe15a11420002
OtiliaNicola <otilianicola94@gmail.com>
2025-04-05 18:44:42 +0200

    fix: add provideHttpClient

--- sigaus-tool/src/app/app.config.ts
+++ sigaus-tool/src/app/app.config.ts
@@ -9,6 +9,7 @@ import {
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
+    provideHttpClient(),
     providePrimeNG({
       // theme: {
       //     preset: Aura
