90bfbe242fd95b3ff7501105fb55efccc45f93aa
OtiliaNicola <otilianicola94@gmail.com>
2025-04-06 22:29:33 +0200

    feat: add provider primeng

--- sigaus-tool/src/app/app.config.ts
+++ sigaus-tool/src/app/app.config.ts
@@ -8,6 +8,7 @@ import {
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { provideHttpClient } from '@angular/common/http';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +17,8 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
     provideHttpClient(),
-    providePrimeNG({}),
+    providePrimeNG({
+      theme: { preset: Aura, options: { darkModeSelector: '.p-dark' } },
+  }),
   ],
 };
