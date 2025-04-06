0a85bd9901e8994cc8003b03cb004b4b7976ee11
OtiliaNicola <otilianicola94@gmail.com>
2025-04-06 08:06:45 +0200

    fix: change function & delete placeholder

--- sigaus-tool/src/app/shared/filter/filter.component.ts
+++ sigaus-tool/src/app/shared/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
@@ -11,14 +11,19 @@ import { InputTextModule } from 'primeng/inputtext';
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
   @Input() years: any[] = [];
   @Input() months: any[] = [];
   @Output() filterChanged = new EventEmitter<any>();
   
-  selectedYear: number | null = null;
+  selectedYear: number = 2025; // Default to 2025
   selectedMonth: number | null = null;
   referenceNumber: string = '';
+
+  ngOnInit(): void {
+    // Emitir los filtros iniciales al cargar el componente
+    this.onFilterChange();
+  }
   
   onFilterChange(): void {
     this.filterChanged.emit({
@@ -26,5 +31,10 @@ export class FilterComponent {
       month: this.selectedMonth,
       reference: this.referenceNumber
     });
+  }
+
+  clearReference(): void {
+    this.referenceNumber = '';
+    this.onFilterChange();
   }
 }
