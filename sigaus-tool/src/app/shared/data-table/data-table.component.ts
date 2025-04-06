d8582ad0b8eae98dc64f27aa725abe107c3c289a
OtiliaNicola <otilianicola94@gmail.com>
2025-04-06 22:28:32 +0200

    fix: review style

--- sigaus-tool/src/app/shared/data-table/data-table.component.ts
+++ sigaus-tool/src/app/shared/data-table/data-table.component.ts
@@ -1,22 +1,35 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, SimpleChanges, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TableModule } from 'primeng/table';
+import { Table, TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { Declaration } from '../../core/interfaces/declaration.interface';
 import { GestionService } from '../../core/services/gestion.service';
 import { FormsModule } from '@angular/forms';
-/* import { CheckboxModule } from 'primeng/checkbox'; */
+import { CheckboxModule } from 'primeng/checkbox';
+import { DropdownModule } from 'primeng/dropdown';
+import { MatIconModule } from '@angular/material/icon';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-data-table',
   standalone: true,
-  imports: [CommonModule, TableModule, ButtonModule],
+  imports: [
+    TableModule,
+    FormsModule,
+    CommonModule,
+    ButtonModule,
+    CheckboxModule,
+    DropdownModule,
+    MatIconModule,
+    PaginatorModule
+  ],
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.scss'],
 })
 export class DataTableComponent {
   @Input() data: Declaration[] = [];
   @Input() loading: boolean = false;
+  @ViewChild('dt') table: Table | undefined;
 
   entriesCount = 0;
   entriesWeight = 0;
@@ -25,7 +38,7 @@ export class DataTableComponent {
 
   selectedDeclarations!: Declaration;
 
-  constructor(private gestionService: GestionService) {}
+  constructor(private readonly gestionService: GestionService) {}
 
   ngOnInit(): void {
     this.calculateStatistics();
@@ -42,29 +55,31 @@ export class DataTableComponent {
     // Calcular entradas
     const entries = this.data.filter((item) => item.type === 'ENTRADA');
     this.entriesCount = entries.length;
-    this.entriesWeight = entries.reduce(
-      (sum, item) => sum + item.quantity,
-      0
-    );
+    this.entriesWeight = entries.reduce((sum, item) => sum + item.quantity, 0);
 
     // Calcular salidas
     const exits = this.data.filter((item) => item.type === 'SALIDA');
     this.exitsCount = exits.length;
-    this.exitsWeight = exits.reduce(
-      (sum, item) => sum + item.quantity,
-      0
-    );
+    this.exitsWeight = exits.reduce((sum, item) => sum + item.quantity, 0);
   }
-
-  // Función auxiliar para extraer el peso de un string como "100 kg"
-  // private extractWeight(cantidad: string): number {
-  //   if (!cantidad) return 0;
-  //   const match = cantidad.toLowerCase().match(/(\d+)/);
-  //   return match ? parseInt(match[1]) : 0;
-  // }
 
   // Método para exportar a Excel
   exportToExcel(): void {
     this.gestionService.exportToExcel(this.data);
+  }
+
+  getStartRecord(): number {
+    return (this.table?.first ?? 0) + 1;
+  }
+  getEndRecord(): number {
+    if (!this.table) return 0;
+    return (this.table?.first ?? 0) + (this.table?.rows ?? 0) >
+      (this.table?.totalRecords ?? 0)
+      ? this.table?.totalRecords ?? 0
+      : (this.table?.first ?? 0) + (this.table?.rows ?? 0);
+  }
+
+  getTotalRecords(): number {
+    return this.table ? this.table.totalRecords : 0;
   }
 }
